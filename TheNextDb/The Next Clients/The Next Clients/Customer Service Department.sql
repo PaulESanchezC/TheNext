e0d74af42a1ada039fcac529617f5da13a563577
TheRealPaulyglot <PaulESanchezC@outlook.com>
2022-05-17 16:01:03 -0400

    Customer Service Department

---
+++ TheNextDb/The Next Clients/The Next Clients/Customer Service Department.sql
@@ -0,0 +1,16 @@
+create table Customers
+(
+	CustomerId int primary key,
+	CustomerFirstName varchar (50) not null,
+	CustomerMiddleName varchar (50) null,
+	CustomerLastName varchar (50) not null,
+	CustomerPhoneNumber varchar (20) not null unique,
+	CustomerEmail varchar(50) not null unique,
+	CustomerStreetAddress varchar(100) not null,
+	CustomerCity varchar(50) not null,
+	CustomerProvince varchar(50) not null,
+
+	--Bussiness logic
+	DateCreated date default getDate(),
+	IsActive bit default 1,
+);
